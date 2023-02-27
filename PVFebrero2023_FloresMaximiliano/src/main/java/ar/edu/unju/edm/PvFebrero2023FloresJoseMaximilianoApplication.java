0d3b46edff70409c509351e72f5162a513fc6d56
PC000 <PC000@DESKTOP-IRHEU6U>
2023-02-27 20:33:14 -0300

    commit inicial

---
+++ PVFebrero2023_FloresMaximiliano/src/main/java/ar/edu/unju/edm/PvFebrero2023FloresJoseMaximilianoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvFebrero2023FloresJoseMaximilianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvFebrero2023FloresJoseMaximilianoApplication.class, args);
+	}
+
+}
